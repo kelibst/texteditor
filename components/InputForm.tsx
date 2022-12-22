14f31b72ba98968167af5659b928733abd174ed5
Keli Booster <kbooster17@gmail.com>
2022-12-22 17:09:10 +0000

    update inputform

--- components/InputForm.tsx
+++ components/InputForm.tsx
@@ -1,46 +1,73 @@
-import React, { FormEvent, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { useText } from "../context";
-import { allowableInterface } from "../interfaces/dataInterface";
+import { allowableInterface, allowedHtml } from "../interfaces/dataInterface";
 import PopUpCard from "./PopUpCard";
 
 const InputForm = (props: { allowable: allowableInterface }) => {
+  // destructuring showPopUp and setshowPopUp from the allowable prop
+
   const { showPopUp, setshowPopUp } = props.allowable;
+  // currentInput state is used to store the value of the input field
   const [currentInput, setCurrentInput] = useState("");
+  // useText hook is used to gain access to the dispatchData function from the context
   const { dispatchData } = useText();
 
+  // handleInputSubmit function is used to handle the form submission event
   const handleInputSubmit = (event: FormEvent) => {
     event.preventDefault();
-    currentInput.length > 1 &&
-      dispatchData({
-        type: "ADD_TEXT",
-        payload: {
-          id: uuidv4(),
-          value: currentInput,
-        },
-      });
-    setCurrentInput("");
+    if (currentInput.length) {
+      console.log(showPopUp, "shopoup");
+      // deal with instances where the person does not enter anything before /1
+      if (showPopUp.type) {
+        dispatchData({
+          type: "ADD_TEXT",
+          payload: {
+            id: uuidv4(),
+            value: currentInput,
+
+            html: `h${currentInput[-1]}}` as allowedHtml,
+          },
+        });
+      } else {
+        dispatchData({
+          type: "ADD_TEXT",
+          payload: {
+            id: uuidv4(),
+            value: currentInput,
+          },
+        });
+      }
+      setCurrentInput("");
+    }
+
     return;
   };
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-
+  const updatePopUpState = () => {
     if (
-      value[value.length - 1] === "/" ||
-      (value[value.length - 2] === "/" && Number(value[value.length - 1]) < 6)
+      currentInput[currentInput.length - 1] === "/" ||
+      (currentInput[currentInput.length - 2] === "/" &&
+        Number(currentInput[currentInput.length - 1]) < 6)
     ) {
       setshowPopUp({
         isPopUp: true,
-        type: Number(value[value.length - 1])
-          ? Number(value[value.length - 1])
+        type: Number(currentInput[currentInput.length - 1])
+          ? Number(currentInput[currentInput.length - 1])
           : 0,
       });
-      setCurrentInput(event.target.value);
     } else {
       showPopUp && setshowPopUp({ isPopUp: false, type: 0 });
-      setCurrentInput(event.target.value);
     }
+  };
+
+  useEffect(() => {
+    updatePopUpState();
+  }, [currentInput]);
+
+  // handleInputChange function is used to handle the input field value change event
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentInput(event.target.value);
   };
 
   return (
